7106705282b70d33bba24985184f13966051791c
Sam Pagenkopf <ssaammp@gmail.com>
2024-02-19 10:09:23 -0600

    initialize "libkon" rust tree library via @rsw

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
+import { ViteRsw } from "vite-plugin-rsw";
 
-export default defineConfig({ start: { ssr: false } });
+export default defineConfig({
+    start: { ssr: false },
+    plugins: [
+        ViteRsw(),
+    ]
+});
